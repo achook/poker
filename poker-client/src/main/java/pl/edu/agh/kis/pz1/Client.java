ca473aa196ca1da3fd77a0e78ddd0312022542cd
achook <karol@kucza.xyz>
2022-11-25 22:54:22 +0100

    Add sockets

--- poker-client/src/main/java/pl/edu/agh/kis/pz1/Client.java
+++ poker-client/src/main/java/pl/edu/agh/kis/pz1/Client.java
@@ -1,7 +1,41 @@
 package pl.edu.agh.kis.pz1;
 
+import java.io.IOException;
+import java.io.Serial;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
+
 public class Client {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static SocketChannel client;
+    private static ByteBuffer buffer;
+
+    public static void start() throws IOException {
+        client = SocketChannel.open(new InetSocketAddress("localhost", 8090));
+        buffer = ByteBuffer.allocate(256);
+    }
+
+    public static String sendMessage(String msg) throws IOException {
+        buffer = ByteBuffer.wrap(msg.getBytes());
+
+        client.write(buffer);
+        buffer.clear();
+
+        client.read(buffer);
+        String response = new String(buffer.array()).trim();
+
+        buffer.clear();
+
+        return response;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        start();
+
+        for (int i = 0; i < 10; i++) {
+            sendMessage(i+"");
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 }
--- poker-server/src/main/java/pl/edu/agh/kis/pz1/Server.java
+++ poker-server/src/main/java/pl/edu/agh/kis/pz1/Server.java
@@ -1,22 +1,39 @@
 package pl.edu.agh.kis.pz1;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Server {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
+    private static ServerSocketChannel serverSocketChannel;
+    private static ServerSocket serverSocket;
+    private static SelectionKey selectionKey;
+    private static Selector selector;
+    private static ByteBuffer buffer;
 
-    private void startServer(int port) throws InternalServerException {
-        try {
-            serverSocket = new ServerSocket(port);
-        } catch (Exception e) {
-            throw new InternalServerException(e.toString());
-        }
+    public static void startServer(int port) throws InternalServerException, IOException {
+        var address = new InetSocketAddress(port);
+
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+
+        serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(address);
+
+        selector = Selector.open();
+
+        selectionKey = serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
     }
 
-    private void stopServer() throws InternalServerException {
+    public static void stopServer() throws InternalServerException {
         try {
             serverSocket.close();
         } catch (Exception e) {
@@ -24,7 +41,33 @@ public class Server {
         }
     }
 
-    public static void main(String[] args) {
+    private static String readAndAnswer(String message, SelectionKey key)
+            throws IOException {
+
+        var buffer = ByteBuffer.allocate(256);
+
+        SocketChannel client = (SocketChannel) key.channel();
+
+        client.read(buffer);
+        String response = new String(buffer.array()).trim();
+        buffer.clear();
+
+        buffer.put(message.getBytes());
+        client.write(buffer);
+        buffer.clear();
+
+        return response;
+    }
+
+    private static void register(Selector selector, ServerSocketChannel serverSocket)
+            throws IOException {
+
+        SocketChannel client = serverSocket.accept();
+        client.configureBlocking(false);
+        client.register(selector, SelectionKey.OP_READ);
+    }
+
+    public static void main(String[] args) throws InternalServerException, IOException {
         Deck d = new Deck();
         Hand h = new Hand();
 
@@ -38,6 +81,27 @@ public class Server {
 
         for (int i = 0; i < 5; i++) {
             System.out.println(cs.get(i).getName());
+        }
+
+        startServer(8090);
+
+        while (true) {
+            selector.select();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iter = selectedKeys.iterator();
+            while (iter.hasNext()) {
+
+                SelectionKey key = iter.next();
+
+                if (key.isAcceptable()) {
+                    register(selector, serverSocketChannel);
+                }
+
+                if (key.isReadable()) {
+                    readAndAnswer("Read", key);
+                }
+                iter.remove();
+            }
         }
     }
 }
